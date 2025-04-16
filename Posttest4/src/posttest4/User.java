b2c7c6f06b24a0a3183dc939b49e610139900ec7
Al Hajj Fauzan <162179863+alhajjfauzan@users.noreply.github.com>
2025-04-16 09:02:13 +0800

    POSTTEST4
    POSTTEST4

---
+++ Posttest4/src/posttest4/User.java
@@ -0,0 +1,24 @@
+package posttest4;
+
+class User {
+    private String nama;
+    private String role;
+    private String password;
+
+    User(String nama, String role, String password) {
+        this.nama = nama;
+        this.role = role;
+        this.password = password;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public String getRole() {
+        return role;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+
+}
